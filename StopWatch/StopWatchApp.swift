a5a82e10ac59197c5334afccd5cff933e091835b
githubuiux <uiuxista@gmail.com>
2023-09-07 12:51:30 +0300

    Initial Commit

---
+++ StopWatch/StopWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopWatchApp.swift
+//  StopWatch
+//
+//  Created by Levent on 7.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct StopWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
